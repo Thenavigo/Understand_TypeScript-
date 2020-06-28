94e41ec64965978ac9847b50589a73f75879a5b5
Patrick BIYAGA <biyagapatrick@gmail.com>
2020-06-28 22:32:09 +0200

    argument types

--- Workspace/app.ts
+++ Workspace/app.ts
@@ -40,3 +40,44 @@ let address: [string, number] = ["Superstreet", 99];
  console.log(myColor);
 
  You see 1 in your console. because number assign automatically.
+
+ // any
+ let car: any = "BWM";
+ console.log(car);
+ car = { brand: "BWM", series: 3};
+ console.log(car);
+
+  You see Object
+
+  // Using Types For A Better Code - Understanding The Created JavaScript Code
+
+  - In order to understand the JavaScript code created, it is important to know beforehand the basics and therefore the types of data to be created and manipulated.
+
+
+// functions
+
+function returnMyname()
+{
+	retrun myName;
+}
+console.log(returnMyname());
+
+You see 'Max'
+
+a character string created at the beginning of this course.
+
+// void
+function sayHello(): void 
+{
+	console.log("Hello");	
+}
+
+// argument types
+function multiply(alue1: number, value2: number): number 
+{
+	return value1 + value2;	
+}
+
+console.log(multiply(2, 'Max'));
+
+You see error because Max is not a number.
