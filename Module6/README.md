725397f81d8b1249ccb1f7449f812f2aefcef69b
Patrick BIYAGA <biyagapatrick@gmail.com>
2020-07-12 22:37:55 +0200

    Loading Modules �

--- Module6/README.md
+++ Module6/README.md
@@ -36,7 +36,7 @@ console.log(MyMath.calculateRectangle(10,20));
 console.log(MyMath.calculateCircumference(3));
 console.log(PI);
 
-// run tsc
+run tsc
 ```
 
 
@@ -78,4 +78,43 @@ and run tsc app.ts --outFile app.js
 3 - Next change console.log() like this -> console.log(CircleMath.calculateCircumference(3));
 4 - Next run run tsc app.ts --outFile app.js
 ```
+
+```bash
+- Limitations Of Namespaces
+ namespaces will hit their end-of-life soon, basically they solved a problem that now can be solved with classical ES6 syntax. Even if the import/export keyword is not working in all browsers out of the box, that’ the road they are covering so it’s just a matter of time and we’ll get rid of any other solution we had to manage complexity.
+```
+
+```bash
+-  Modules
+let's create the math directory with these two (circle, rectangle).ts  inside.
+console.log(PI);
+console.log(calculateCircumference(10));
+run tsc You see "Uncaught ReferenceError: require is not defined"
+// console.log()
+```
+
+```bash
+- Loading Modules 👇
+
+[Learn more about module loader](https://github.com/systemjs/systemjs) 
+
+1 -  install systemjs like this ->  npm i systemjs --save
+
+2 - in our index.html add these two scripts js
+
+<script src="node_modules/systemjs/dist/system.js"></script>
+&
+<script>
+	// set our baseURL reference path
+	SystemJS.config({
+		baseURL: '/',
+		defaultJSExtensions: true
+	});
+	System.import('app.js');
+</script>
+
+and run tsc
+```
+
+
 
--- Module6/app.ts
+++ Module6/app.ts
@@ -1,7 +1,9 @@
-/// <reference path="circleMath.ts" />
-/// <reference path="rectangleMath.ts" />
+import { PI, calculateCircumference } from "./math/circle"; 
 
-import CircleMath = MyMath.Circle;
+//   /// <reference path="circleMath.ts" />
+//   /// <reference path="rectangleMath.ts" />
+
+// import CircleMath = MyMath.Circle;
 
 // Namespaces and Modules - An Introduction To Namespaces
 
@@ -17,11 +19,11 @@ import CircleMath = MyMath.Circle;
 //	}
 //}
 
-const PI = 2.99;
+// const PI = 2.99;
 
-console.log(MyMath.calculateRectangle(10,20));
-console.log(CircleMath.calculateCircumference(3));
-console.log(PI);
+// console.log(MyMath.calculateRectangle(10,20));
+// console.log(CircleMath.calculateCircumference(3));
+// console.log(PI);
 
 // run tsc
 
@@ -52,3 +54,35 @@ console.log(PI);
 // Next change console.log() like this -> console.log(CircleMath.calculateCircumference(3));
 // Next run run tsc app.ts --outFile app.js
 
+
+// Limitations Of Namespaces
+// namespaces will hit their end-of-life soon, basically they solved a problem that now can be solved with classical ES6 syntax. Even if the import/export keyword is not working in all browsers out of the box, that’ the road they are covering so it’s just a matter of time and we’ll get rid of any other solution we had to manage complexity.
+
+
+// Modules
+// let's create the math directory with these two (circle, rectangle).ts  inside.
+console.log(PI);
+console.log(calculateCircumference(10));
+// run tsc You see "Uncaught ReferenceError: require is not defined"
+// console.log()
+
+
+// Loading Modules 👇
+
+// [Learn more about module loader](https://github.com/systemjs/systemjs) 
+
+// install systemjs like this ->  npm i systemjs --save
+
+// in our index.html add these two scripts js
+
+// <script src="node_modules/systemjs/dist/system.js"></script>
+// <script>
+	// set our baseURL reference path
+	//SystemJS.config({
+	//	baseURL: '/',
+	//	defaultJSExtensions: true
+//	});
+//	System.import('app.js');
+//</script>
+
+// and run tsc
