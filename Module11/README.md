8bec0dcfa838e0c8a4cc082957898c4802cf59ca
Patrick BIYAGA <biyagapatrick@gmail.com>
2020-07-22 22:02:43 +0200

    Adding TypeScript into a Webpack Workflow

--- Module11/README.md
+++ Module11/README.md
@@ -68,5 +68,98 @@ tsc -p config/tsconfig.json
 ```bash
  - Adding TypeScript Into A Gulp Workflow
 
- 
+ 1 - install gulp-typescript like this -> npm i --save-dev gulp gulp-typescript
+
+ 2 - create gulpfile.js and write this body content
+
+
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("typescript", function() {
+	return tsProject.src()
+		.pipe(ts(tsProject))
+		.pipe(gulp.dest(""));
+});
+
+gulp.task("watch", function() {
+	gulp.watch("*.ts", ["typescript"]);
+});
+
+gulp.task("default", ["watch"]);
+
+
+3 - configure your package.json like this:
+
+ "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "lite-server",
+    "build": "gulp"
+  },
+
+
+ 4 - do -> npm run build
+
+
+ 5 - official documentation : https://www.npmjs.com/package/gulp-typescript
+```
+
+
+```bash
+ -  Adding TypeScript Into A Webpack Workflow
+
+
+ 1 - install webpack like this -> npm i --save-dev webpack ts-loader
+
+
+ 2 - create file webpack.config.js and write this body content
+
+ module.exports = {
+	entry: "./app.ts",
+	output: {
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{ test: /\.ts$/, loader: "ts-loader"}
+		]
+	}
+};
+
+
+ 3- configure Your package.json like this :
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "lite-server",
+    "build": "webpack -d --watch",
+    "build:prod": "webpack -p"
+  },
+
+
+ 4 - run  npm install --save-dev typescript
+
+
+ 5 - You also need to have a tsconfig.json file. For example
+
+ {
+  "compilerOptions": {
+    "target": "ES5",
+    "module": "system",
+    "moduleResolution": "node",
+    "sourceMap": true,
+    "emitDecoratorMetadata": true,
+    "experimentalDecorators": true,
+    "removeComments": false,
+    "noImplicitAny": false
+  },
+  "exclude": [
+    "node_modules",
+    ".npm"
+  ]
+}
+
+
+6 - run npm run build
 ```
---
+++ Module11/dist/bundle.js
@@ -0,0 +1,100 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./app.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./app.ts":
+/*!****************!*\
+  !*** ./app.ts ***!
+  \****************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("(void 0)([\"jquery\"], function (exports_1, context_1) {\r\n    \"use strict\";\r\n    var $;\r\n    var __moduleName = context_1 && context_1.id;\r\n    return {\r\n        setters: [\r\n            function ($_1) {\r\n                $ = $_1;\r\n            }\r\n        ],\r\n        execute: function () {\r\n            $(\"#app\").css({ \"background-color\": \"blue\" });\r\n        }\r\n    };\r\n});\r\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9hcHAudHMuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hcHAudHM/NTZjNSJdLCJzb3VyY2VzQ29udGVudCI6WyJTeXN0ZW0ucmVnaXN0ZXIoW1wianF1ZXJ5XCJdLCBmdW5jdGlvbiAoZXhwb3J0c18xLCBjb250ZXh0XzEpIHtcclxuICAgIFwidXNlIHN0cmljdFwiO1xyXG4gICAgdmFyICQ7XHJcbiAgICB2YXIgX19tb2R1bGVOYW1lID0gY29udGV4dF8xICYmIGNvbnRleHRfMS5pZDtcclxuICAgIHJldHVybiB7XHJcbiAgICAgICAgc2V0dGVyczogW1xyXG4gICAgICAgICAgICBmdW5jdGlvbiAoJF8xKSB7XHJcbiAgICAgICAgICAgICAgICAkID0gJF8xO1xyXG4gICAgICAgICAgICB9XHJcbiAgICAgICAgXSxcclxuICAgICAgICBleGVjdXRlOiBmdW5jdGlvbiAoKSB7XHJcbiAgICAgICAgICAgICQoXCIjYXBwXCIpLmNzcyh7IFwiYmFja2dyb3VuZC1jb2xvclwiOiBcImJsdWVcIiB9KTtcclxuICAgICAgICB9XHJcbiAgICB9O1xyXG59KTtcclxuIl0sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7Iiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./app.ts\n");
+
+/***/ })
+
+/******/ });
